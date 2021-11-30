0049acd03f99b2dc30a469d453ad7ccdb005c72e
louiseslb <louise.saliba99@gmail.com>
2021-11-30 10:18:21 +0100

    actions for restart and StartSession

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -1,27 +1,27 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, SessionStarted, ActionExecuted, EventType, Restarted
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionSessionStart(Action):
+    def name(self) -> Text:
+        return "action_session_start"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        events = [SessionStarted(), ActionExecuted("action_listen")]
+        dispatcher.utter_message(
+            text='Bonjour, je suis AbotHotel votre assistant virtuel. Je suis la pour vous aider à faire une réservation.')
+
+        return events
+
+
+class ActionRestarted(Action):
+    """ This is for restarting the chat"""
+
+    def name(self):
+        return "action_restart"
+
+    def run(self, dispatcher, tracker, domain):
+        return [Restarted()]
